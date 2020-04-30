f3fa9453b8cd7f1f5afdcbe4d4b5d664acc6a3a0
Clement <clement.cheradame@free.fr>
2020-04-30 13:26:22 +0200

    Finalisation de l'appli

--- src/app/Service/score.service.ts
+++ src/app/Service/score.service.ts
@@ -9,10 +9,10 @@ export class ScoreService {
   constructor(private http: HttpClient) { }
 
   post(score ){
-    return this.http.post('https://af45a576.ngrok.io/scores', score);
+    return this.http.post('https://a2ad4a84.ngrok.io/scores', score);
   }
   get(){
-    return this.http.get('https://af45a576.ngrok.io/scores');
+    return this.http.get('https://a2ad4a84.ngrok.io/scores');
   }
 }
 
